f7868aa10a4efeead2d173f8b6606a57a7d1804d
Luis Enrique <luisenriquecgongora@gmail.com>
2019-10-19 14:07:03 +0200

    Add code to show time series

--- sample_scripts/librosa_sample.py
+++ sample_scripts/librosa_sample.py
@@ -1,5 +1,7 @@
 import librosa
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 from playsound import playsound
 
 x, sr = librosa.load('worldholdon_bobsinclair.mp3')
@@ -13,6 +15,15 @@ print("GENERAL ANALYSIS")
 mins = math.floor(x.shape[0]/(sr*60))
 secs = math.floor((x.shape[0]/sr)%(60))
 print('Duration - ', '{}:{}'.format(mins,secs))
-print()
+print("ARRAY SAMPLE")
 
-playsound('worldholdon_bobsinclair.mp3')
+print("Time Series")
+t = np.arange(x.shape[0])/sr
+#print(t)
+
+
+plt.plot(t,x)
+plt.ylabel('Time Series')
+plt.show()
+
+#playsound('worldholdon_bobsinclair.mp3')
