9239efe604125ef322905e6b52e8c4d8ac011cd1
Luis Enrique <luisenriquecgongora@gmail.com>
2019-10-19 15:27:01 +0200

    Ready spectogram

--- sample_scripts/librosa_sample.py
+++ sample_scripts/librosa_sample.py
@@ -2,6 +2,7 @@ import librosa
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import librosa.display
 from playsound import playsound
 
 x, sr = librosa.load('worldholdon_bobsinclair.mp3')
@@ -20,16 +21,18 @@ print("ARRAY SAMPLE")
 print("Time Series")
 t = np.arange(x.shape[0])/sr
 
+# plt.figure(figsize=(14, 5))
+# librosa.display.waveplot(x, sr=sr)
+# plt.show()
+# plt.plot(t,x)
+# plt.ylabel('Time Series')
 
 X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
+Xdb = librosa.amplitude_to_db(abs(X),ref=np.max)
+print(Xdb.shape)
 plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.colorbar()
-
-
-plt.plot(t,x)
-plt.ylabel('Time Series')
+librosa.display.specshow(Xdb, y_axis='log', x_axis='time')
+plt.title('Power spectrogram')
+plt.colorbar(format='%+2.0f dB')
+plt.tight_layout()
 plt.show()
-
-#playsound('worldholdon_bobsinclair.mp3')
